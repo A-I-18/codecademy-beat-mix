8cb4a9955fe7d7d9a8ba51f551e9cb230f0335ad
A-I-18 <antonio.ionta.18@gmail.com>
2025-07-21 11:12:31 +0200

    feat: implement preset handler

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,25 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (requestType, presetsIndex, newPresetArray) => {
+    if (requestType !== 'GET' && requestType !== 'PUT') {
+        return [400];
+    }
+    if (presets[presetsIndex] === undefined) {
+        return [404];
+    }
+
+    let response = [200];
+
+    if (requestType === 'GET') {
+        response.push(presets[presetsIndex]);
+    } else if (requestType === 'PUT') {
+        presets[presetsIndex] = newPresetArray;
+        response.push(newPresetArray);
+    }
+
+    return response;
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
