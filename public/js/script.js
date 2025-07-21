5630f4294f8a34ff6472dccc3f93edde2a235776
A-I-18 <antonio.ionta.18@gmail.com>
2025-07-21 10:48:24 +0200

    feat: implement toggle drum functionality

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,23 @@ let snares = Array.from({ length: 16 }, () => false);
 let hiHats = Array.from({ length: 16 }, () => false);
 let rideCymbals = Array.from({ length: 16 }, () => false);
 
+function toggleDrum(arrayName, index) {
+    if (index < 0 || index > 15) { // invalid array index
+        return;
+    }
+
+    switch (arrayName) {
+        case 'kicks':
+            kicks[index] = !kicks[index];
+            break;
+        case 'snares':
+            snares[index] = !snares[index];
+            break;
+        case 'hiHats':
+            hiHats[index] = !hiHats[index];
+            break;
+        case 'rideCymbals':
+            rideCymbals[index] = !rideCymbals[index];
+            break;
+    }
+}
